3c95acbff739628dfb0e19f0fe6895c8551c8608
Ghifaryh <mhghifaryy@gmail.com>
2022-07-20 14:19:26 +0700

    testing, add bootstrap

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About"
+    ]);
 });
