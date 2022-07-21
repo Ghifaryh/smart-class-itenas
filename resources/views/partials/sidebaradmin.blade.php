02abca1576f6fcd0b178a8bcede69f29b375cb81
Arif Rosyid Prasetyio <aprasetyio@gmail.com>
2022-07-21 11:06:07 +0700

    penambahan sidebar

---
+++ resources/views/partials/sidebaradmin.blade.php
@@ -0,0 +1,5 @@
+{{-- <div id="mySidenav" class="sidenav sticky-top"> --}}
+<div id="mySidenav" class="sidenav">
+    <a href="/" id="home" class="textw"><i class="fa-solid fa-house logowsidebaro"></i>Home</a>
+    <a href="" id="daftar" class="asettext"><i class="fa-solid fa-pen-to-square logowsidebarw"></i>Daftar Ruangan</a>
+</div>
