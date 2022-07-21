348ddb978289d6333464cfc563b5d4d747a9bbf6
Ghifaryh <mhghifaryy@gmail.com>
2022-07-21 10:25:12 +0700

    Added Navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,27 +4,27 @@
             <img src="https://www.itenas.ac.id/wp-content/uploads/2021/01/Varian-Logo-Itenas-FULL-02.png" alt=""
                 width="10%">
             <span class="titlewb mx-3 fs-4 fw-bold">SMART CLASS ITENAS</span>
-            {{-- <p class="d-flex">SMART CLASS ITENAS</p> --}}
         </a>
-
-        <div class="user-stuff d-flex">
-
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex">
+            {{-- <ul class="navbar-nav me-auto mb-2 d-flex"> --}}
             <li class="nav-item dropdown">
-                <p>Komrud</p>
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
                     data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="fa-solid fa-user text-danger"></i>
+                    <span>
+                        <i class="fa-solid fa-user text-danger"></i>
+                        <span>Komrud</span>
+                    </span>
                 </a>
-                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Logout</a>
-                </div>
+
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">Status Pemesanan</a></li>
+                    <li><a class="dropdown-item" href="#">Logout</a></li>
+                </ul>
             </li>
-            <span>
+            {{-- <span class="d-flex"> --}}
+            {{-- </span> --}}
 
-                <img src="https://www.itenas.ac.id/wp-content/uploads/2020/07/Logo-id-Heebo.png" width="5%"
-                    alt="">
-            </span>
-        </div>
-
+        </ul>
+        <img src="https://www.itenas.ac.id/wp-content/uploads/2020/07/Logo-id-Heebo.png" width="3%" alt="">
     </div>
 </nav>
