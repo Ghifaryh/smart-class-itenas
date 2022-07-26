f0039670ca2fb3ff7ffdc3fe739e9e95404b3acf
Ghifaryh <mhghifaryy@gmail.com>
2022-07-26 15:37:47 +0700

    adding show hide js

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,8 +21,7 @@
     <script src="scripts/script.js"></script>
 
     {{-- JQ --}}
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-        crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     {{-- icon --}}
     <script src="https://kit.fontawesome.com/57a5f9b5e4.js" crossorigin="anonymous"></script>
