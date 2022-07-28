97b20d1cddeed1e4064b26648acb1e5310f1fd3b
Arif Rosyid Prasetyio <aprasetyio@gmail.com>
2022-07-28 15:25:22 +0700

    menambahkan fitur login dan registrasi

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -11,5 +12,19 @@ class LoginController extends Controller
         return view('ujicoba.login', [
             'title' => 'Login'
         ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'email' => ['required','email:dns'],
+            'password' => ['required']
+
+        ]);
+
+        dd('berhasil login!');
+        // return view('ujicoba.login', [
+        //     'title' => 'Login'
+        // ]);
     }
 }
