09fe0a1454dafa8edf6e318b08868d488ab7d078
Arif Rosyid Prasetyio <aprasetyio@gmail.com>
2022-08-02 11:22:25 +0700

    memperbaiki tampilan login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,7 +9,7 @@ class LoginController extends Controller
 {
     public function index()
     {
-        return view('ujicoba.login', [
+        return view('login', [
             'title' => 'Login'
         ]);
     }
