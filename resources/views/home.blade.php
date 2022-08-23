a15ebab3828a164eeac8e13a1aeb67a1eaccf077
Arif Rosyid Prasetyio <aprasetyio@gmail.com>
2022-08-23 16:11:18 +0700

    tambahan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -82,10 +82,8 @@ function bulanIndo($hariInggris)
     <div class="container container-sm mb-4" id="">
         <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
             <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="{{ $numton }}"
-                    aria-current="true" aria-label="Slide {{ $numton }}"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="{{ $numton }}" class="active"
-                    aria-current="true" aria-label="Slide {{ $numton }}"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
                     aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
