111b3c4c99169d40b299729ddb0e18c98dad8e4d
Arif Rosyid Prasetyio <aprasetyio@gmail.com>
2022-08-26 10:43:29 +0700

    suruh gib

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        // $datenext = Carbon::now()->addMonth()->format('Y-m-d');
+        $datenext = Carbon::now()->addWeek()->format('Y-m-d');
         $date = Carbon::now()->format('Y-m-d');
         // dd($date);
 
@@ -23,12 +23,12 @@ class HomeController extends Controller
         
         return view('home', [
             "title" => "Home",
-            "jdwlrgn1" => Jadwal::where('id_ruangan', '=', 1)->where('tanggal_pinjam', '>=', $date)->get(),
-            "jdwlrgn2" => Jadwal::where('id_ruangan', '=', 2)->where('tanggal_pinjam', '>=', $date)->get(),
-            "jdwlrgn3" => Jadwal::where('id_ruangan', '=', 3)->where('tanggal_pinjam', '>=', $date)->get(),
-            "jdwlrgn4" => Jadwal::where('id_ruangan', '=', 4)->where('tanggal_pinjam', '>=', $date)->get(),
-            "jdwlrgn5" => Jadwal::where('id_ruangan', '=', 5)->where('tanggal_pinjam', '>=', $date)->get(),
-            "jdwlrgn6" => Jadwal::where('id_ruangan', '=', 6)->where('tanggal_pinjam', '>=', $date)->get(),
+            "jdwlrgn1" => Jadwal::where('id_ruangan', '=', 1)->where('tanggal_pinjam', '>=', $date)->where('tanggal_pinjam', '<=', $datenext)->get(),
+            "jdwlrgn2" => Jadwal::where('id_ruangan', '=', 2)->where('tanggal_pinjam', '>=', $date)->where('tanggal_pinjam', '<=', $datenext)->get(),
+            "jdwlrgn3" => Jadwal::where('id_ruangan', '=', 3)->where('tanggal_pinjam', '>=', $date)->where('tanggal_pinjam', '<=', $datenext)->get(),
+            "jdwlrgn4" => Jadwal::where('id_ruangan', '=', 4)->where('tanggal_pinjam', '>=', $date)->where('tanggal_pinjam', '<=', $datenext)->get(),
+            "jdwlrgn5" => Jadwal::where('id_ruangan', '=', 5)->where('tanggal_pinjam', '>=', $date)->where('tanggal_pinjam', '<=', $datenext)->get(),
+            "jdwlrgn6" => Jadwal::where('id_ruangan', '=', 6)->where('tanggal_pinjam', '>=', $date)->where('tanggal_pinjam', '<=', $datenext)->get(),
             "ruangan" => Ruangan::count(),
         ]);
     }
